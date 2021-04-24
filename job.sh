c5452b16539a08f03f151326558a3dd02a302dfb
grieger <grieger@ds2.ad.fh-aachen.de>
2021-04-24 17:36:18 +0200

    corrected results

--- job.sh
+++ job.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 python scripts/transform_files_tuebingen.py -e exp001
+#python scripts/run_experiment.py -e exp001
 python scripts/evaluate_experiment.py -e exp001 -d valid
+python scripts/evaluate_experiment.py -e exp001 -d test
